acce51d9c5e72ae38a52d11385a81f47a1b18463
castberry10 <soc09072@gmail.com>
2022-03-30 04:03:42 +0900

    ch3

---
+++ ch3-node-basic/5-internal-module/7-worker_treads/prime.js
@@ -0,0 +1,26 @@
+//스레드를 사용하지 않는 소수 탐색색 예제
+const min = 2;
+const max = 10000000;
+const primes = [];
+
+function findPrimes(start, range){
+    let isPrime = true;
+    const end = start + range;
+    for (let i = start; i < end; i++){
+        for(let j = min; j<Math.sqrt(end); j++){
+            if(i !== j && i % j === 0 ){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            primes.push(i);
+        }
+        isprime = true;
+    }
+}
+
+console.time('prime');
+findPrimes(min, max);
+console.timeEnd('prime');
+console.log(primes.length);
