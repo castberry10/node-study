c1b1a30a881124adbf546f004b819f3be27eeb38
castberry10 <soc09072@gmail.com>
2022-04-28 00:46:44 +0900

    aa

--- ch9-express-sns/2-db/models/post.js
+++ ch9-express-sns/2-db/models/post.js
@@ -1,5 +1,26 @@
 const Sequelize = request(Sequelize);
 
 module.exports = class Post extends Sequelize.module{
-    
-}
+    static init(sequelize) {
+        return super.init({
+            content: {
+                type: Sequelize.STRING(140),
+                allowNull: false,
+            },
+            img:{
+                type:Sequelize.STRING(200),
+                allowNull: true,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+    static associate(db) 
+};
