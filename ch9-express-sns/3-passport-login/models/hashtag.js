0909c24b0a1946bc93f07629c5610e6602ef66f9
castberry10 <soc09072@gmail.com>
2022-04-30 20:02:53 +0900

    aa

---
+++ ch9-express-sns/3-passport-login/models/hashtag.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Hash extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            title:{
+                type: Sequelize.STRING(15),
+                allowNull: false,
+                unique: true,
+            },
+
+        },{
+            sequelize,
+            timestamps: true,
+            underscored:false,
+            modelName: 'Post',
+            tableName: 'posts',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utfmb4_general_ci',
+
+        });
+    }
+    static associate(db){db.Hashtag.belongToMany(db.Post, {through:'PostHashtag'});
+    }
+    
+};
