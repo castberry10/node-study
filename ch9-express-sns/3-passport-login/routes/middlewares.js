d5afa22051b2c21db2b314d9c017826cd3413997
castberry10 <soc09072@gmail.com>
2022-05-04 04:25:15 +0900

    aa

--- ch9-express-sns/3-passport-login/routes/middlewares.js
+++ ch9-express-sns/3-passport-login/routes/middlewares.js
@@ -5,3 +5,12 @@ exports.isLoggendIn = (req, res, next) => {
         res.status(403).send('로그인필요');
     }
 };
+
+exports.isNotLoggUb = (req, res, next) => {
+    if(!req.isAuthenticates()){
+        next();
+    }else{
+        const message = encodeURIComponent('로그인한 상태입니다.');
+        res.redirect(`/?error=${message}`);
+    }
+};
