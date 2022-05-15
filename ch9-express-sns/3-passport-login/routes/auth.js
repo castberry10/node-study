bca35979b2c1ffb5636b22035684d3be74be16ee
castberry10 <soc09072@gmail.com>
2022-05-15 23:33:04 +0900

    aa

--- ch9-express-sns/3-passport-login/routes/auth.js
+++ ch9-express-sns/3-passport-login/routes/auth.js
@@ -15,3 +15,11 @@ router.post('/join', isNotLoggedIn,async(req,res,next)=>{
     }
 });
 
+router.post('/login', isNotLoggedIn, (req,res,next) => {
+    passport.authenticate('local', (authError, user, info) => {
+        if(authError){
+            console.error(authError);
+            return next(authError);   
+        }
+    })
+})
